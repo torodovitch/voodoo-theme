445741b6808c751722e0e4ef96b778642dc052e1
torodovitch <torodovitch@gmail.com>
2020-06-06 11:48:00 -0400

    add jsdoc @param and js constant and typeof

--- demos/javascript-demo.js
+++ demos/javascript-demo.js
@@ -5,10 +5,15 @@ const CONST_VALUE = "some string value";
 
 console.warn(`Formatted console warning printing the above ${CONST_VALUE}.`);
 
-let foo = bar;
+let array = [];
+let other_array = [];
 
 for (let index = 0; index < array.length; index++) {
-	const element = array[index];
+	if (typeof array[index] === "string") {
+		const element = array[index];
+	}
+
+	other_array.push(element);
 }
 
 export default class JsDemoObject {
@@ -34,4 +39,64 @@ export default class JsDemoObject {
 
 		return colour.toLowerCase();
 	};
+
+	/**
+	 * Updates the topbar's transparency state.
+	 * @param {boolean} isTransparent whether transparency should be enabled or not.
+	 */
+	setTransparency(isTransparent) {
+		this.isTransparent = isTransparent;
+	}
+}
+
+/** @namespace */
+class JSDocDemoClass {
+	/**
+	 * Solves equations of the form a * x = b
+	 * @example <caption>Example usage of method1.</caption>
+	 * // returns 2
+	 * globalNS.method1(5, 10);
+	 * @returns {Number} Returns the value of x for the equation.
+	 */
+	method1(a, b) {
+		return b / a;
+	}
+
+	/** @constructor */
+	JSDocDemoClass() {
+		/** A module. Its name is module:foo/bar.
+		 * @module foo/bar
+		 */
+		/** The built in string object. Its name is external:String.
+		 * @external String
+		 */
+		/** An event. Its name is module:foo/bar.event:MyEvent.
+		 * @event module:foo/bar.event:MyEvent
+		 */
+	}
+
+	chat = {
+		/**
+		 * Refer to this by {@link chat."#channel"}.
+		 * @namespace
+		 */
+		"#channel": {
+			/**
+			 * Refer to this by {@link chat."#channel".open}.
+			 * @type {boolean}
+			 * @defaultvalue
+			 */
+			open: true,
+			/**
+			 * Internal quotes have to be escaped by backslash. This is
+			 * {@link chat."#channel"."say-\"hello\""}.
+			 */
+			'say-"hello"': function (msg) {},
+		},
+	};
+
+	/**
+	 * Now we define an event in our {@link chat."#channel"} namespace.
+	 * @event chat."#channel"."op:announce-motd"
+	 */
 }
